d6fcb57bef3f28f239be01fdb017fc295fa19cc1
thoadao0301 <daolebaothoa123@gmail.com>
2022-05-12 22:27:32 +0700

    init

---
+++ src/main/java/com/englishtutor/EnglishTutorApplication.java
@@ -0,0 +1,13 @@
+package com.englishtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishTutorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/englishtutor/model/Tutor.java
@@ -0,0 +1,4 @@
+package com.englishtutor.model;
+
+public class Tutor {
+}
