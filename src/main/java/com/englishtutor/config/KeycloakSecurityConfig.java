d36516335bb18209ab5a46c85081cf2ed07c907d
Thoa Đào <thoadao@MacBook-Air.local>
2023-02-07 14:47:44 +0100

    add config

--- src/main/java/com/englishtutor/config/KeycloakSecurityConfig.java
+++ src/main/java/com/englishtutor/config/KeycloakSecurityConfig.java
@@ -1,80 +1,75 @@
-//package com.englishtutor.config;
-//
-//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-//import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-//import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-//import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-//import org.springframework.security.core.session.SessionRegistryImpl;
-//import org.springframework.security.web.AuthenticationEntryPoint;
-//import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-//import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-//import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
-//
-//@KeycloakConfiguration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(jsr250Enabled = true)
-//public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        super.configure(http);
-//
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .anyRequest()
-//                .permitAll()
-//                .and()
-//                .addFilterBefore(new SimpleCORSFilter(), WebAsyncManagerIntegrationFilter.class)
-//                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
-//                .and()
-//                .logout()
-//                .addLogoutHandler(keycloakLogoutHandler())
-//                .logoutUrl("/user/logout").permitAll()
-//                .invalidateHttpSession(true)
-//                .clearAuthentication(true)
-//                .deleteCookies("JSESSIONID");
-//    }
-//
-//    /**
-//     * Registers the KeycloakAuthenticationProvider with the authentication manager
-//     */
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-//        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
-////        simpleAuthorityMapper.setPrefix("");
-//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
-//        auth.authenticationProvider(keycloakAuthenticationProvider);
-//
-//    }
-//
-//    /**
-//     * Defines the session authentication strategy
-//     *
-//     * @return null
-//     */
-//    @Bean
-//    @Override
-//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-//        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-//    }
-//
-//    @Bean
-//    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
-//        return new KeycloakSpringBootConfigResolver();
-//    }
-//
-//    protected AuthenticationEntryPoint authenticationEntryPoint() throws Exception {
-//        return new RestAuthenticationEntryPoint(adapterDeploymentContext());
-//    }
-//
-//}
+package com.englishtutor.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
+
+@KeycloakConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        super.configure(http);
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .addFilterBefore(new SimpleCORSFilter(), WebAsyncManagerIntegrationFilter.class)
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler())
+                .logoutUrl("/user/logout").permitAll()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID");
+    }
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+//        simpleAuthorityMapper.setPrefix("");
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+
+    }
+
+    /**
+     * Defines the session authentication strategy
+     *
+     * @return null
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    protected AuthenticationEntryPoint authenticationEntryPoint() throws Exception {
+        return new RestAuthenticationEntryPoint(adapterDeploymentContext());
+    }
+
+}
